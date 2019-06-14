81c91c4f8a837f6d4cfc35a66fc165e2c5016acf
Your Name <315214192@qqq.com>
2019-06-14 08:35:41 -0400

    Test alluxio java client download big file

--- src/main/java/TestAlluxio.java
+++ src/main/java/TestAlluxio.java
@@ -23,11 +23,11 @@ public class TestAlluxio {
 //        ((InstancedConfiguration) configuration).set(PropertyKey.MASTER_HOSTNAME,"3.89.38.106");
 //        ClientContext.create(configuration);
         FileSystem fs = FileSystem.Factory.get();
-        AlluxioURI path = new AlluxioURI("/Staging/1.txt");
+        AlluxioURI path = new AlluxioURI("/UserSpace/1-3/unet_membrane.hdf5");
 
         FileInStream in = fs.openFile(path);
 
-        FileOutputStream fileOutputStream = new FileOutputStream("/root/test.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream("/root/unet_membrane.hdf5");
         fileOutputStream.write(IOUtils.toByteArray(in));
         in.close();
         fileOutputStream.close();
